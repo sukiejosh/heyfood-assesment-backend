74cce174b0a7aeb52d743f7866b00929015102b5
sukiejosh <oladokunjoshua2016@gmail.com>
2025-09-06 19:58:40 +0100

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://your-frontend-domain.com']
+    ? ['https://joshua-heyfood-assessment-frontend.vercel.app']
     : ['http://localhost:3000', 'http://127.0.0.1:3000']
 }));
 app.use(morgan('combined'));
